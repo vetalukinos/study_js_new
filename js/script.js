d317e10935b37a67c97f9369dce94ef559484016
@vitaliylukin91 <vitaliylukin91@gmail.com>
2020-04-02 20:54:56 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -5,35 +5,43 @@ let isNumber = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-
-
-
-function one() {
-    let x = 10;
-    let sum;
-
-    function two(y) {
-        y = prompt('Угадай число от 1 до 100');
-        if (y > x) {
-            return ('Загаданное число меньше');
-        } else if (y < x) {
-            return ('Загаданное число больше');
-        } else if (!isNumber(y))  {
-            return ('Введи число!');
-        } else if (y === null) {
-            return console.log(y);
-        }
-        sum += y; // ВОТ ЭТА СТРОКА СКОРЕЕ ВСЕГО ФИГНЯ КАКАЯ-ТО
-        return sum; //
-    }
-    return two();
+function randomize() {
+    return Math.ceil(Math.random() * 10);
 }
 
-alert(one());
+function one() {
+    let x = randomize();
+    let start = confirm('Ты хочешь чыгрфть в игру?');
+    if (start === true) {
+        two();
+    } else {
+        alert ('Пока!');
+    }
+    function two() {
+        let y = prompt('Угадай число от 1 до 100');
+        if (!isNumber(y)) {
+            alert('Введи число!');
+            two();
+        }
+        y = +y;
+        if (y > x) {
+            alert ('Загаданное число меньше');
+            two();
+        } else if (y < x) {
+            alert ('Загаданное число больше');
+            two();
+        } else if (y === x) {
+           alert('Ты выиграл!')
+            return;
+        }
+    }
+}
+
+one();
 
 
 
-
+//Заменить на тернарники
 
 
 
