b3b683e057bfd2e96eef2d8b620b4d8e14831990
@vitaliylukin91 <vitaliylukin91@gmail.com>
2020-04-02 19:05:43 +0300

    lesson 06

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+
+let isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+
+
+
+function one() {
+    let x = 10;
+    let sum;
+
+    function two(y) {
+        y = prompt('Угадай число от 1 до 100');
+        if (y > x) {
+            return ('Загаданное число меньше');
+        } else if (y < x) {
+            return ('Загаданное число больше');
+        } else if (!isNumber(y))  {
+            return ('Введи число!');
+        } else if (y === null) {
+            return console.log(y);
+        }
+        sum += y; // ВОТ ЭТА СТРОКА СКОРЕЕ ВСЕГО ФИГНЯ КАКАЯ-ТО
+        return sum; //
+    }
+    return two();
+}
+
+alert(one());
+
+
+
+
+
+
+
